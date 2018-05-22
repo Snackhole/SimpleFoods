c4f0075edc753aa5321f9042ec6c4d4f4529d3a2
Snackhole <35538327+Snackhole@users.noreply.github.com>
2018-05-22 10:54:15 -0500

    All recipes done and tested; localization done

--- src/main/java/com/snackhole/simplefoods/items/ModItems.java
+++ src/main/java/com/snackhole/simplefoods/items/ModItems.java
@@ -4,16 +4,16 @@ import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
-    public static ItemFoodBase porkSandwich = new ItemFoodBase("porksandwich", 10, (float) 12, false);
-    public static ItemFoodBase beefSandwich = new ItemFoodBase("beefsandwich", 10, (float) 12, false);
-    public static ItemFoodBase muttonSandwich = new ItemFoodBase("muttonsandwich", 10, (float) 12, false);
-    public static ItemFoodBase salmonSandwich = new ItemFoodBase("salmonsandwich", 10, (float) 12, false);
-    public static ItemFoodBase fishSandwich = new ItemFoodBase("fishsandwich", 10, (float) 12, false);
-    public static ItemFoodBase chickenSandwich = new ItemFoodBase("chickensandwich", 10, (float) 12, false);
-    public static ItemFoodBase rabbitSandwich = new ItemFoodBase("rabbitsandwich", 10, (float) 12, false);
-    public static ItemFoodBase overloadedPotato = new ItemFoodBase("overloadedpotato", 10, (float) 12, false);
-    public static ItemFoodBase roastedCarrot = new ItemFoodBase("roastedcarrot", 5, (float) 5.4, false);
-    public static ItemFoodBase bakedApple = new ItemFoodBase("bakedapple", 6, (float) 3.6, false);
+    public static ItemFoodBase porkSandwich = new ItemFoodBase("pork_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase beefSandwich = new ItemFoodBase("beef_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase muttonSandwich = new ItemFoodBase("mutton_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase salmonSandwich = new ItemFoodBase("salmon_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase fishSandwich = new ItemFoodBase("fish_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase chickenSandwich = new ItemFoodBase("chicken_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase rabbitSandwich = new ItemFoodBase("rabbit_sandwich", 10, (float) 12, false);
+    public static ItemFoodBase overloadedPotato = new ItemFoodBase("overloaded_potato", 10, (float) 12, false);
+    public static ItemFoodBase roastedCarrot = new ItemFoodBase("roasted_carrot", 5, (float) 5.4, false);
+    public static ItemFoodBase bakedApple = new ItemFoodBase("baked_apple", 6, (float) 3.6, false);
 
     public static void register(IForgeRegistry<Item> registry) {
         registry.registerAll(porkSandwich, beefSandwich, muttonSandwich, salmonSandwich, fishSandwich, chickenSandwich, rabbitSandwich, overloadedPotato, roastedCarrot, bakedApple);
